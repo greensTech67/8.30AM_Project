57dede2e9d95a23960340609c05d4993f9305480
nivethanu75 <nivethanu75@gmail.com>
2020-02-04 12:51:12 +0530

    nivetha new code

--- src/org/cts/test/scan.java
+++ src/org/cts/test/scan.java
@@ -1,38 +1,47 @@
 package org.cts.test;
 
-
-
 public class scan {
 	public static void main(String[] args) {
+
+		byte age = 22;
+		System.out.println("My age is:" + age);
+
+		int regno = 987654321;
+		System.out.println("my reg no is :" + regno);
+
+		short id = 4444;
+		System.out.println("my id is :" + id);
+
+		char initial = 'A';
+		System.out.println("my initial is:" + initial);
+
+		float sal = 25.5f;
+		System.out.println("my sal is:" + sal);
+
+		double avg = 22;
+		System.out.println("my avg is:" + avg);
+
+		long phnno = 9876543210l;
+		System.out.println("my phn no is:" + phnno);
+
+		String name = "Greens";
+		System.out.println("my name is:" + name);
+
+		boolean isEmployee = true;
+		System.out.println("i am employee:" + isEmployee);
+		System.out.println("Raseem");
+		System.out.println("Raseem");
+		System.out.println("Raseem");
+		System.out.println("Raseem");
+		System.out.println("Raseem");
+
+		System.out.println("Raseem");
 		
-		byte age=22;
-		System.out.println("My age is:"+age);
+		System.out.println("nivetha");
 		
-		int regno=987654321;
-		System.out.println("my reg no is :"+regno);
+		System.out.println("nivetha new");
 		
-		short id=4444;
-		System.out.println("my id is :"+id);
 		
-		char initial='A';
-		System.out.println("my initial is:"+initial);
-	
-	    float sal=25.5f;
-	    System.out.println("my sal is:"+sal);
-	    
-	    double avg=22;
-	    System.out.println("my avg is:"+avg);
-	    
-	    long phnno=9876543210l;
-	    System.out.println("my phn no is:"+phnno);
-	    
-	    String name="Greens";
-	    System.out.println("my name is:"+name);
-	    
-	    boolean isEmployee=true;
-	    System.out.println("i am employee:"+isEmployee);
-	    
 	}
-	
 
 }
